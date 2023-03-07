1455803fcf6de458224eba8640d77388b49e73e5
Matthew Dias <matthewdias13@gmail.com>
2023-03-06 21:37:18 -0800

    Created VideoList components.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,39 +11,23 @@ import react.dom.html.ReactHTML.img
 import react.dom.client.createRoot
 import kotlinx.serialization.Serializable
 
+data class Video(
+    val id: Int,
+    val title: String,
+    val speaker: String,
+    val videoUrl: String
+)
+
+val unwatchedVideos = listOf(
+    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+)
+
+val watchedVideos = listOf(
+    Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+)
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
-    createRoot(container).render(Fragment.create {
-        h1 {
-            +"KotlinConf Explorer"
-        }
-        div {
-            h3 {
-                +"Videos to watch"
-            }
-            p {
-                + "John Doe: Building and breaking things"
-            }
-            p {
-                +"Jane Smith: The development process"
-            }
-            p {
-                +"Matt Miller: The Web 7.0"
-            }
-            h3 {
-                +"Videos watched"
-            }
-            p {
-                +"Tom Jerry: Mouseless development"
-            }
-        }
-        div {
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-            }
-        }
-    })
+    createRoot(container).render(App.create())
 }
