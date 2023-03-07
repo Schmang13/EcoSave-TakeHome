9699585f717ceea8ad227fd27634d5c8165e5764
Matthew Dias <matthewdias13@gmail.com>
2023-03-06 23:08:43 -0800

    Finished Kotlin/React web app tutorial.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,20 +1,44 @@
 import kotlinx.coroutines.async
 import react.*
-import react.dom.*
 import kotlinx.browser.window
 import kotlinx.coroutines.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import emotion.react.css
-import csstype.Position
-import csstype.px
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.h3
 import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.p
-import react.dom.html.ReactHTML.img
+
+val mainScope = MainScope()
+
+suspend fun fetchVideo(id: Int): Video {
+    val response = window
+        .fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+        .await()
+        .text()
+        .await()
+    return Json.decodeFromString(response)
+}
+
+suspend fun fetchVideos(): List<Video> = coroutineScope {
+    (1..25).map { id ->
+        async {
+            fetchVideo(id)
+        }
+    }.awaitAll()
+}
 
 val App = FC<Props> {
+    var currentVideo: Video? by useState(null)
+    var currentAudio: Video? by useState(null)
+    var unwatchedVideos: List<Video> by useState(emptyList())
+    var watchedVideos: List<Video> by useState(emptyList())
+
+    useEffectOnce {
+        mainScope.launch {
+            unwatchedVideos = fetchVideos()
+        }
+    }
+
     h1 {
         +"KotlinConf Explorer"
     }
@@ -24,25 +48,39 @@ val App = FC<Props> {
         }
         VideoList{
             videos = unwatchedVideos
+            selectedVideo = currentVideo
+            onSelectVideo = { video ->
+                currentVideo = video
+                currentAudio = video
+            }
         }
         h3 {
             +"Videos watched"
         }
         VideoList{
             videos = watchedVideos
+            selectedVideo = currentVideo
+            onSelectVideo = { video ->
+                currentVideo = video
+                currentAudio = video
+            }
         }
     }
     div {
-        css {
-            position = Position.absolute
-            top = 10.px
-            right = 10.px
-        }
-        h3 {
-            +"John Doe: Building and breaking things"
-        }
-        img {
-            src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+        currentVideo?.let { curr ->
+            VideoPlayer {
+                video = curr
+                unwatchedVideo = curr in unwatchedVideos
+                onWatchedButtonPressed = {
+                    if (video in unwatchedVideos) {
+                        unwatchedVideos = unwatchedVideos - video
+                        watchedVideos = watchedVideos + video
+                    } else {
+                        watchedVideos = watchedVideos - video
+                        unwatchedVideos = unwatchedVideos + video
+                    }
+                }
+            }
         }
     }
 }
--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,21 +1,20 @@
-import kotlinx.browser.window
 import react.*
-import react.dom.*
 import react.dom.html.ReactHTML.p
 
 external interface VideoListProps : Props {
     var videos: List<Video>
+    var selectedVideo: Video?
+    var onSelectVideo: (Video) -> Unit
 }
 
 val VideoList = FC<VideoListProps> {props ->
-    var selectedVideo: Video? by useState(null)
     for (video in props.videos) {
         p {
             key = video.id.toString()
             onClick = {
-                selectedVideo = video
+                props.onSelectVideo(video)
             }
-            if (video == selectedVideo) {
+            if (video == props.selectedVideo) {
                 +"▶ "
             }
             +"${video.speaker}: ${video.title}"
